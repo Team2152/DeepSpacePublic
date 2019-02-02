db76e9eff36bfa3ccca33bc1d1dbeca72923af08
kennethbchen <kennethbchen@gmail.com>
2019-02-02 13:04:49 -0500

    Added Documentation to SharedButton.java

--- src/main/java/frc/robot/utilities/SharedButton.java
+++ src/main/java/frc/robot/utilities/SharedButton.java
@@ -1,10 +1,43 @@
 package frc.robot.utilities;
 
 import edu.wpi.first.wpilibj.buttons.Button;
+ /**
+  * This class is used to allow the same <Code>Command</code> to be excecuted by different 
+  * <code>Buttons</code> 
+
+  * <p> This class should be used like a <code>Button</code> is used in <code>OI.java</code>
+	<p> <b>Example:</b>
+	<p> <pre>
+private Button dButtonA = new Button(ID);
+
+
+	public void setupDriverXboxButtons() {
+		dButtonA.whenReleased(new ExampleCommand();
+}
+	</pre>
+
+	<p> <b>Should be replaced with:</b>
+	<pre>
+private Button dButtonA = new Button(ID);
+private Button dButtonB = new Button(ID);
+
+private SharedButton button = 
+	new SharedButton(new Button[] {dButtonA,dButtonB});
+
+	public void setupSharedCommands() {
+		button.whenReleased(new ExampleCommand());
+  }
+	</pre>
+  */
+
 public class SharedButton extends Button {
 	
 	private Button[] buttons;
 	
+	/**
+	 * Constructor for SharedButton.java
+	 * @param buttons array of button classes to share one command
+	 */
 	public SharedButton(Button[] buttons){
 		this.buttons = buttons;
 	}
